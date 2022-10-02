e1b6d6ddc44b82af7e0b41d095ab2329b240e596
Martín (Netux) Rodríguez <send.email@netux.site>
2022-10-02 10:59:36 -0300

    Implement Banned Beatmap Characteristics setting.
    Disallow play counting for beatmaps difficulties with these characteristics.
    Defaults to only "Lightshow".
    
    Does not have an UI yet.

--- BeatmapPlayCount/Configuration/PluginConfig.cs
+++ BeatmapPlayCount/Configuration/PluginConfig.cs
@@ -1,6 +1,10 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using IPA.Config;
+using IPA.Config.Data;
 using IPA.Config.Stores;
+using IPA.Config.Stores.Attributes;
+using IPA.Config.Stores.Converters;
 
 [assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
 namespace BeatmapPlayCount.Configuration
@@ -9,7 +13,11 @@ namespace BeatmapPlayCount.Configuration
     {
         public static PluginConfig Instance { get; set; }
 
-        // Must be 'virtual' if you want BSIPA to detect a value change and save the config automatically.
+        // Members must be 'virtual' if we want BSIPA to detect a value change and save the config automatically.
+
         public virtual float MinimumSongProgressToIncrementingPlayCount { get; set; } = 0.7f;
+
+        [NonNullable, UseConverter(typeof(CollectionConverter<string, HashSet<string>>))]
+        public virtual HashSet<string> BannedBeatmapCharacteristics { get; set; } = new HashSet<string> { "Lightshow" };
     }
 }
--- BeatmapPlayCount/Managers/TrackPlaytime.cs
+++ BeatmapPlayCount/Managers/TrackPlaytime.cs
@@ -1,4 +1,5 @@
 ﻿using BeatmapPlayCount.Configuration;
+using System.Linq;
 using Zenject;
 
 namespace BeatmapPlayCount.Managers
@@ -7,25 +8,33 @@ namespace BeatmapPlayCount.Managers
     {
         private readonly AudioTimeSyncController audioSyncController;
         private readonly string beatmapId;
+        private readonly string beatmapCharacteristic;
 
         public bool Incremented { get; private set; }
         public bool IsGameplayAnExternalModReplay { get; private set; }
+        public bool DoesBeatmapHaveBannedCharacteristic { get; private set; }
 
         public TrackPlaytime(IDifficultyBeatmap _currentlyPlayingLevel, AudioTimeSyncController _audioSyncController)
         {
             audioSyncController = _audioSyncController;
             beatmapId = _currentlyPlayingLevel.level.levelID;
+            beatmapCharacteristic = _currentlyPlayingLevel.parentDifficultyBeatmapSet.beatmapCharacteristic.serializedName;
         }
 
         public void Initialize()
         {
             Incremented = false;
             IsGameplayAnExternalModReplay = Utils.ExternalReplayMod.CheckIfGameplayIsAReplay();
+            DoesBeatmapHaveBannedCharacteristic = PluginConfig.Instance.BannedBeatmapCharacteristics
+                .Any(bannedCharacteristic => bannedCharacteristic == beatmapCharacteristic);
+#if DEBUG
+            Plugin.Log.Info($"TrackPlaytime IsGameplayAnExternalModReplay = {IsGameplayAnExternalModReplay}; DoesBeatmapHaveBannedCharacteristic = {DoesBeatmapHaveBannedCharacteristic}");
+#endif
         }
 
         public void Tick()
         {
-            if (IsGameplayAnExternalModReplay || Incremented)
+            if (IsGameplayAnExternalModReplay || DoesBeatmapHaveBannedCharacteristic || Incremented)
             {
                 return;
             }
